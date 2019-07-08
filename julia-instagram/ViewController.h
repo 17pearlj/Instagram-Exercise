460563065c2a59f4b50a0e8d4cd69d3321e79a3a
Julia Pearl <jpearl@fb.com>
2019-07-08 15:11:58 -0700

    Initial Commit

---
+++ julia-instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  julia-instagram
+//
+//  Created by jpearl on 7/8/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ julia-instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  julia-instagram
+//
+//  Created by jpearl on 7/8/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
